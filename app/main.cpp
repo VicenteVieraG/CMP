95a114014732e0dd8d9fa17d50366aea12486b3a
Vicente Viera <plantedcoot706@gmail.com>
2024-03-20 08:38:49 -0600

    Project Configured

---
+++ app/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <cstdlib>
+
+#include <config.hpp>
+
+int main(int argc, char** argv){
+    std::cout<<project_name<<std::endl;
+    std::cout<<project_version<<std::endl;
+
+    std::system("pause");
+
+    return 0;
+}
