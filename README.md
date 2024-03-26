a8e01886e13135a8e9dfba74ef14762f30319035
Vicente Viera <plantedcoot706@gmail.com>
2024-03-26 15:03:54 -0600

    Create Directory Function for Windows Implemented

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
-# CMP
+# CPM
 Created by [Vicente Viera](https://github.com/VicenteVieraG)
-## About the CMP
+## About the CPM
 This is a package manager inspired by NPM. The goal of this project is to make working with C/C++ libraries as easy as it is with NPM.
 
 The objective is to create an abstraction layer over CMake, so no previous knowledge about CMake is needed to build and run proffessional C/C++ projects. It should be as easy as to create a project in C/C++ with a command and get right into the code without the need of extra configuration. If any library is required, it should follow the same principle of simplicity and cero CMake previous knoledge.
--- app/main.cpp
+++ app/main.cpp
@@ -1,13 +1,40 @@
 #include <iostream>
-#include <cstdlib>
+#include <cstring>
+#include <windows.h>
 
 #include <config.hpp>
+
+void createDir(const char* dirName);
 
 int main(int argc, char** argv){
     std::cout<<project_name<<std::endl;
     std::cout<<project_version<<std::endl;
 
-    std::system("pause");
-
     return 0;
+}
+
+void createDir(const char* dirName){
+    TCHAR pwd[MAX_PATH];
+    if(!GetCurrentDirectory(MAX_PATH, pwd)){
+        std::cerr<<"Error Getting Current Directory: "<<pwd<<'\\'<<std::endl;
+
+        return;
+    }
+
+    size_t dirLen = sizeof(pwd) + sizeof(dirName) + 2;
+
+    char* path = new char[dirLen];
+    path[0] = '\0';
+
+    strcat(path, pwd);
+    strcat(path, "\\");
+    strcat(path, dirName);
+
+    if(!CreateDirectory(path, NULL)){
+        std::cerr<<"Error Creating Directory: "<<path<<std::endl;
+
+        return;
+    }
+
+    delete path;
 }
