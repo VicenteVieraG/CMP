6c1a0c31d73ee77d3d26666f49fd711dc615fcdd
Vicente Javier Viera Guízar <plantedcoot706@gmail.com>
2024-06-08 15:23:23 -0600

    createFile function implemented

--- app/main.cpp
+++ app/main.cpp
@@ -5,12 +5,11 @@
 #include <config.hpp>
 
 bool createDir(const std::string dirName);
+bool createFile(const std::string fileName);
 
 int main(int argc, char** argv){
     std::cout<<project_name<<std::endl;
     std::cout<<project_version<<std::endl;
-
-    createDir("TEST");
 
     return 0;
 }
@@ -29,5 +28,25 @@ bool createDir(const std::string dirName){
         return false;
     }
 
+    return true;
+}
+
+bool createFile(const std::string filePath){
+    HANDLE file = CreateFile(
+        filePath.c_str(),
+        GENERIC_WRITE,
+        0,
+        NULL,
+        CREATE_NEW,
+        FILE_ATTRIBUTE_NORMAL,
+        NULL
+    );
+
+    if(file == INVALID_HANDLE_VALUE){
+        std::cerr<<"-- Error Creating File: "<<filePath<<std::endl<<GetLastError()<<std::endl;
+        return false;
+    }
+
+    CloseHandle(file);
     return true;
 }
