241da86c946d0c6bf590fe08cbed81a0fbaa495a
Vicente Javier Viera Guízar <plantedcoot706@gmail.com>
2024-06-08 17:15:52 -0600

    initProject folder structure creation implemented

--- app/main.cpp
+++ app/main.cpp
@@ -1,52 +1,91 @@
 #include <iostream>
+#include <stdexcept>
 #include <string>
 #include <windows.h>
 
 #include <config.hpp>
 
-bool createDir(const std::string dirName);
-bool createFile(const std::string fileName);
+#define ROOT_DIR std::string(CPM::getCWD() + "\\")
+
+namespace CPM {
+    std::string getCWD();
+    bool createDir(const std::string dirName);
+    bool createFile(const std::string fileName);
+    void initProject();
+};
+
+const std::string DIRECTORIES[5] = {
+    "app",
+    "config",
+    "external",
+    "include",
+    "src"
+};
 
 int main(int argc, char** argv){
     std::cout<<project_name<<std::endl;
     std::cout<<project_version<<std::endl;
 
+    CPM::initProject();
+
     return 0;
 }
 
-bool createDir(const std::string dirName){
-    TCHAR cwd[MAX_PATH];
-    if(!GetCurrentDirectory(MAX_PATH, cwd)){
-        std::cerr<<"-- Error Getting Current Directory"<<std::endl;
-        return false;
+namespace CPM {
+    std::string getCWD(){
+        TCHAR cwd[MAX_PATH];
+        if(!GetCurrentDirectory(MAX_PATH, cwd)){
+            std::cerr<<"-- Error Getting Current Directory"<<std::endl;
+            return "\0";
+        }
+        return std::string(cwd);
     }
 
-    const std::string path = std::string(cwd) + "\\" + dirName;
+    bool createDir(const std::string dirName){
+        const std::string path = ROOT_DIR + dirName;
 
-    if(!CreateDirectory(path.c_str(), NULL)){
-        std::cerr<<"-- Error Creating Directory: "<<path<<std::endl;
-        return false;
+        if(!CreateDirectory(path.c_str(), NULL)){
+            std::cerr<<"-- Error Creating Directory: "<<path<<std::endl;
+            return false;
+        }
+
+        return true;
     }
 
-    return true;
-}
+    bool createFile(const std::string fileName){
+        HANDLE file = CreateFile(
+            std::string(ROOT_DIR + fileName).c_str(),
+            GENERIC_WRITE,
+            0,
+            NULL,
+            CREATE_NEW,
+            FILE_ATTRIBUTE_NORMAL,
+            NULL
+        );
 
-bool createFile(const std::string filePath){
-    HANDLE file = CreateFile(
-        filePath.c_str(),
-        GENERIC_WRITE,
-        0,
-        NULL,
-        CREATE_NEW,
-        FILE_ATTRIBUTE_NORMAL,
-        NULL
-    );
+        if(file == INVALID_HANDLE_VALUE){
+            std::cerr<<"-- Error Creating File: "<<fileName<<"\n"<<GetLastError()<<std::endl;
+            return false;
+        }
 
-    if(file == INVALID_HANDLE_VALUE){
-        std::cerr<<"-- Error Creating File: "<<filePath<<std::endl<<GetLastError()<<std::endl;
-        return false;
+        CloseHandle(file);
+        return true;
     }
 
-    CloseHandle(file);
-    return true;
-}
+    void initProject(){
+        try{
+            // Create folder structure
+            for(const std::string dir : DIRECTORIES){
+                if(!createDir(dir)) throw std::runtime_error("-- Error Creating directory: " + ROOT_DIR + dir);
+                if(!createFile(dir + "\\CMakeLists.txt")) throw std::runtime_error("-- Error Creating File: " + ROOT_DIR + dir + "\\CMakeLists.txt");
+            }
+        }catch(const std::runtime_error& error){
+            std::cerr<<"-- Error Initializing Project"<<std::endl;
+            std::cerr<<error.what()<<std::endl;
+            return;
+        }catch(...){
+            std::cerr<<"-- Unknown Error Initializig Project"<<std::endl;
+            return;
+        }
+    }
+};
